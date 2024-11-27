688fc8623c91166e54ed8c5325727d1e73eeeede
Jurriaan Roelofs <jur@dxpr.com>
2024-11-27 17:26:08 +0100

    chore: update devtools configuration

--- scripts/run-drupal-check.sh
+++ scripts/run-drupal-check.sh
@@ -2,16 +2,7 @@
 set -vo pipefail
 
 DRUPAL_RECOMMENDED_PROJECT=${DRUPAL_RECOMMENDED_PROJECT:-10.3.x-dev}
-PHP_EXTENSIONS="gd"
 DRUPAL_CHECK_TOOL="mglaman/drupal-check"
-
-# Install required PHP extensions
-for ext in $PHP_EXTENSIONS; do
-  if ! php -m | grep -q $ext; then
-    apk update && apk add --no-cache ${ext}-dev
-    docker-php-ext-install $ext
-  fi
-done
 
 # Create Drupal project if it doesn't exist
 if [ ! -d "/drupal" ]; then
@@ -31,8 +22,10 @@ if [[ $DRUPAL_RECOMMENDED_PROJECT == 11.* ]]; then
   composer require drupal/statistics
 fi
 
-# Install drupal-check
-composer require $DRUPAL_CHECK_TOOL --dev
+# Install drupal-check only if not already installed
+if [ ! -f "./vendor/bin/drupal-check" ]; then
+  composer require $DRUPAL_CHECK_TOOL --dev
+fi
 
 # Run drupal-check
 ./vendor/bin/drupal-check --drupal-root . -ad web/modules/contrib/ckeditor_ai_assist
